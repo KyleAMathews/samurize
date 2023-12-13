5824ac85bb660053a2b81d99769a00a2f0020a98
Kyle Mathews <mathews.kyle@gmail.com>
2023-12-12 18:22:22 -0800

    More improvements

--- src/routes/_index.tsx
+++ src/routes/_index.tsx
@@ -3,6 +3,8 @@ import { useVideos, useCreateYoutubeVideo } from "../daos/youtube_videos"
 import { Link, useNavigate } from "react-router-dom"
 import Typography from "@mui/material/Typography"
 import Stack from "@mui/material/Stack"
+import List from "@mui/material/List"
+import ListItem from "@mui/material/ListItem"
 import Button from "@mui/material/Button"
 import TextField from "@mui/material/TextField"
 import { Helmet } from "react-helmet-async"
@@ -50,7 +52,9 @@ export default function Index() {
         }}
       >
         <Stack spacing={1} mb={3}>
-          <Typography variant="h1">Summerize a YouTube video</Typography>
+          <Typography variant="h1" pb={1}>
+            AI-powered YouTube Summarization
+          </Typography>
           <TextField
             error={errorMessage != null}
             required
@@ -63,18 +67,55 @@ export default function Index() {
           </Button>
         </Stack>
       </form>
-
       <Stack spacing={1}>
-        <Typography variant="h3">Recent videos</Typography>
-        {videos &&
-          videos.map((video) => {
-            return (
-              <div key={video.id}>
-                <Link to={`/video/${video.id}`}>{video.title}</Link>
-              </div>
-            )
-          })}
+        <Typography variant="h3">Try a few examples</Typography>
+        <List
+          sx={{
+            listStyleType: `disc`,
+            "& .MuiListItem-root": {
+              display: `list-item`,
+              p: 0,
+              ml: 3,
+            },
+          }}
+        >
+          <ListItem>
+            <Link to={`/video/PUv66718DII`}>
+              Bret Victor - Inventing on Principle
+            </Link>
+          </ListItem>
+          <ListItem>
+            <Link to={`/video/yJDv-zdhzMY`}>
+              The Mother of All Demos, presented by Douglas Engelbart (1968)
+            </Link>
+          </ListItem>
+          <ListItem>
+            <Link to={`/video/MnrJzXM7a6o`}>
+              Steve Jobs introduces iPhone in 2007
+            </Link>
+          </ListItem>
+          <ListItem>
+            <Link to={`/video/lK8gYGg0dkE`}>
+              President Franklin D. Roosevelt Declares War on Japan
+            </Link>
+          </ListItem>
+          <ListItem>
+            <Link to={`/video/j3mhkYbznBk`}>
+              Richard Feynman - The.Character of Physical Law - Part 1 The Law
+              of Gravitation
+            </Link>
+          </ListItem>
+        </List>
       </Stack>
+      <div>
+        This site is built with{` `}
+        <a href="https://electric-sql.com/">ElectricSQL</a>
+        {` `}
+        and is open source.{` `}
+        <a href="https://github.com/KyleAMathews/samurize">
+          View code on GitHub.
+        </a>
+      </div>
     </Stack>
   )
 }
